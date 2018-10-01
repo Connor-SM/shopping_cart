ea68430f9635c0c1ed572593c43d6319cb167e25
Connor Milliken <cmilliken09@gmail.com>
2018-10-01 13:42:06 -0400

    Remove product from cart working correctly, TODO: don't show cart if cart is empty

--- static/js/app.js
+++ static/js/app.js
@@ -78,6 +78,7 @@ function showCart() {
         <td>${cart[i].title}</td>
         <td>$${cart[i].price}</td>
         <td>$${cart[i].price}</td>
+        <td><button onclick="removeFromCart(${cart[i].id})" class="btn btn-danger">x</button></td>
       </tr>
     `;
   }
@@ -85,6 +86,26 @@ function showCart() {
   console.log(cart);
   // inject html variable into table-body
   $("#table-body").html(html);
+}
+
+
+// TODO: create remove functionality
+function removeFromCart(id) {
+  // loop through products in cart and remove one instance of id
+  for (let i = 0; i < cart.length; i++) {
+    // check current product for id passed in
+    if (cart[i].id == id) {
+      // splice current product
+      cart.splice(i, 1);
+      break;
+    }
+  }
+
+  // run showCart to generate current cart array
+  // Usage!
+  sleep(50).then(() => {
+    showCart();
+  });
 }
 
 
